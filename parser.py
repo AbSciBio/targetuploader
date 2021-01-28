117c5673283dc6d8a1d992b021417c5801806e83
crudnicky <crudnicky@abscibio.com>
2021-01-28 10:14:32 -0800

    Updated Fasta files and prints to excel sheet.

--- parser.py
+++ parser.py
@@ -13,24 +13,15 @@ class SubunitParser():
         subunit_array = []
         for i in range(3, self.max_col + 1, 2):
             subunit_name = self.ws.cell(self.row, i).value 
-            fasta_description = self.ws.cell(self.row, i + 1).value
-            fasta_file = self.parse_file_name(subunit_name, fasta_description)
-            
             subunit = {
                 "subunit_name": subunit_name,
                 "copies": '1',
-                "amino_acid_fasta_description": fasta_description,
-                "amino_acid_sequence": self.getFastaFileSequence(fasta_file, "AA"),
-                "genes": self.parseGenes(fasta_file)
+                "amino_acid_fasta_description": subunit_name,
+                "amino_acid_sequence": self.getFastaFileSequence(subunit_name, "AA"),
+                "genes": self.parseGenes(subunit_name)
             }
             subunit_array.append(subunit)
         return subunit_array
-  
-    def parse_file_name(self, subunit_name, fasta_description):
-        old_name = subunit_name.split('_')
-        gene_name = old_name[0] + '_' + old_name[2]
-        file_name = gene_name + '_' + fasta_description
-        return file_name
     
     def parseGenes(self, file_name):
         gene = {}
--- ptdb_request.py
+++ ptdb_request.py
@@ -1,8 +1,9 @@
 import requests
 import json
+import pprint
 
 URL = "http://localhost:8000/api/v1/absci-targets/target-registration/"
-TOKEN = ""
+TOKEN = "b94705a1e7169c061c8e550c8416f0c1cbfa0ed7"
 
 
 class PTDBRequest():
@@ -13,8 +14,7 @@ class PTDBRequest():
     headers = self.get_headers()
     json_target_data = self.target
     # request = requests.post(URL, data=json_target_data, headers=headers)
-    # print(request.json())
-    print(json_target_data)
+    # pprint.pprint(json_target_data)
 
   def get_headers(self):
     return {
