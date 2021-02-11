330dc94c9f5d37964eb0ce6bc5c645f344ce7aa7
crudnicky <crudnicky@abscibio.com>
2021-02-11 11:35:42 -0800

    WIP

--- lib/ptdb_request.py
+++ lib/ptdb_request.py
@@ -1,19 +1,22 @@
 import json
 import requests
-
-URL = "http://localhost:8000/api/v1/absci-targets/target-registration/"
+from pprint import pprint
 class PTDBRequest():
   def __init__(self, new_target, user_input):
     self.target = json.dumps(new_target)
     self.url = user_input['API_URL']
+    self.token = user_input['token']
+    pprint(self.target, indent=2)
+    print(" ")
 
   def post_target(self):
     headers = self.get_headers()
-    # request = requests.post(self.url, data=self.target, headers=headers)
+    request = requests.post(self.url, data=self.target, headers=headers)
+    pprint(request.content)
     
   def get_headers(self):
     return {
-      "Authorization": f"Token {TOKEN}",
+      "Authorization": f"Token {self.token}",
       "Accept": "*/*",
       "Content-Type": "application/json"
     }
